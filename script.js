0bc6ce59cc780397e6b42aef51c512879a5dc888
Jian <jianhui9998@gmail.com>
2021-05-09 18:49:36 +0800

    update

--- script.js
+++ script.js
@@ -5,7 +5,34 @@ $(document).ready(function() {
         }else{
             $('.navbar').removeClass('sticky');
         }
+
+        if(this.scrollY > 800){
+            $('.scroll-up-btn').addClass('show');
+        }else{
+            $('.scroll-up-btn').removeClass('show');
+        }
     });
+
+    // slide-up script 
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop:0});
+    });
+
+    // typing animation script 
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Developer", "Backend Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: ["Software Engineer", "Developer", "Backend Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     // toggle menu/navbar script
     $('.menu-btn #burger').click(function() {
         $('.navbar .menu').toggleClass("active");
@@ -20,4 +47,11 @@ $(document).ready(function() {
         $('.menu-btn #burger').css("display", "block");
         $('.menu-btn #close').css("display", "none");
     }); 
+
+    $('.menu li a').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn img').toggleClass("active");
+        $('.menu-btn #burger').css("display", "block");
+        $('.menu-btn #close').css("display", "none");
+    });
 });
